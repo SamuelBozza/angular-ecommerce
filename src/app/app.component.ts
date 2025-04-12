da709f60d2f46af6da18f1a46773c09f93ff394b
Samuel <samuel.bozza0023@outlook.com>
2025-04-11 23:22:10 -0300

    Projeto finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [HeaderComponent, RouterOutlet],
+  template: `
+  <app-header />
+  <router-outlet />
+  `,
+  styleUrl: './components/header/header.component.scss'
 })
 export class AppComponent {
-  title = 'angular-ecommerce';
+
 }
